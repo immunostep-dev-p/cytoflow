1c3d1301186c8b717297d0eae20cce28e211f170
immunostep-dev-p <practicas_s@immunostep.com>
2024-03-13 12:48:10 +0100

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -8,10 +8,12 @@ tube = cytoflow.Tube(file = f)
 
 import_op = cytoflow.ImportOp(tubes = [tube], channels = {"R1-A" : "R1-A", "B8-A" : "B8-A"})
 experiment = import_op.apply()
+experiment
+# print(experiment)
 
-r2d = cytoflow.Range2DOp(name = "Test", xchannel = "R1-A", ychannel = "B8-A")
+range2d = cytoflow.Range2DOp(name = "Test", xchannel = "R1-A", ychannel = "B8-A")
 
-r2d.default_view(xscale = "logicle", yscale = "logicle", interactive = True).plot(experiment)
+range2d.default_view(xscale = "logicle", yscale = "logicle", interactive = True).plot(experiment)
 cytoflow.DensityView(xchannel = "R1-A", xscale = "logicle", ychannel = "B8-A", yscale = "logicle").plot(experiment, gridsize = 100, smoothed = True)
 
 kmeans = cytoflow.KMeansOp(name = "KMeans", channels = ["R1-A", "B8-A"], scale = {"R1-A" : "logicle", "B8-A" : "logicle"}, num_clusters = 5)
@@ -19,40 +21,40 @@ kmeans.estimate(experiment)
 experiment_kmeans = kmeans.apply(experiment)
 kmeans.default_view().plot(experiment_kmeans)
 
-gauss = cytoflow.GaussianMixtureOp(name = "Gauss", channels = ["R1-A", "B8-A"], scale = {"R1-A" : "logicle", "B8-A" : "logicle"}, num_components = 2)
+gaussian_mixture = cytoflow.GaussianMixtureOp(name = "GaussianMixture", channels = ["R1-A", "B8-A"], scale = {"R1-A" : "logicle", "B8-A" : "logicle"}, num_components = 2)
 
-gauss.estimate(experiment)
-experiment_gauss = gauss.apply(experiment)
+gaussian_mixture.estimate(experiment)
+experiment_gaussian_mixture = gaussian_mixture.apply(experiment)
 
-gauss.default_view().plot(experiment_gauss, alpha = 0.1)
+gaussian_mixture.default_view().plot(experiment_gaussian_mixture, alpha = 0.1)
 
 new_experiment = cytoflow.ImportOp(tubes = [cytoflow.Tube(file = f)]).apply()
 new_experiment
 # print(new_experiment)
 
-flow_peaks = cytoflow.FlowPeaksOp(name = "FP", channels = ["R1-A", "B8-A"], scale = {"R1-A" : "logicle", "B8-A" : "logicle"}, h0 = 0.8)
+flow_peaks = cytoflow.FlowPeaksOp(name = "FlowPeaks", channels = ["R1-A", "B8-A"], scale = {"R1-A" : "logicle", "B8-A" : "logicle"}, h0 = 0.8)
 
 flow_peaks.estimate(new_experiment)
-new_experiment2 = flow_peaks.apply(new_experiment)
-flow_peaks.default_view().plot(new_experiment2)
-cytoflow.ScatterplotView(xchannel = "R1-A", xscale = "logicle", ychannel = "B8-A", yscale = "logicle", huefacet = "FP").plot(new_experiment2)
+new_experiment_flow_peaks = flow_peaks.apply(new_experiment)
+flow_peaks.default_view().plot(new_experiment_flow_peaks)
+cytoflow.ScatterplotView(xchannel = "R1-A", xscale = "logicle", ychannel = "B8-A", yscale = "logicle", huefacet = "FlowPeaks").plot(new_experiment_flow_peaks)
 
-new_experiment2.data
-# print(new_experiment2.data)
+new_experiment_flow_peaks.data
+# print(new_experiment_flow_peaks.data)
 
-count = new_experiment2[["FP"]].groupby(by = new_experiment2["FP"]).count()
+count = new_experiment_flow_peaks[["FlowPeaks"]].groupby(by = new_experiment_flow_peaks["FlowPeaks"]).count()
 count
 # print(count)
 
-umbral = abs(new_experiment2["R1-A"].max() / new_experiment2["R1-A"].min())
+umbral = abs(new_experiment_flow_peaks["R1-A"].max() / new_experiment_flow_peaks["R1-A"].min())
 umbral
 # print(umbral)
 
-indice = new_experiment2["R1-A"].loc[new_experiment2["R1-A"] < umbral].index
+indice = new_experiment_flow_peaks["R1-A"].loc[new_experiment_flow_peaks["R1-A"] < umbral].index
 indice
 # print(indice)
 
-cluster = new_experiment2[["FP"]].drop(indice).groupby(by = new_experiment2["FP"].drop(indice)).count()
+cluster = new_experiment_flow_peaks[["FlowPeaks"]].drop(indice).groupby(by = new_experiment_flow_peaks["FlowPeaks"].drop(indice)).count()
 cluster
 # print(cluster)
 
@@ -60,7 +62,7 @@ pcluster = cluster.index[numpy.argmax(cluster.to_numpy())]
 pcluster
 # print(pcluster)
 
-bead_cluster = new_experiment2[new_experiment2["FP"] == pcluster]
+bead_cluster = new_experiment_flow_peaks[new_experiment_flow_peaks["FlowPeaks"] == pcluster]
 bead_cluster
 # print(bead_cluster)
 
