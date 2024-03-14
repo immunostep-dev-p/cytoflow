23278e6e1467079c9a9c0ca9b2f0c998ad773821
immunostep-dev-p <practicas_s@immunostep.com>
2024-03-14 13:51:25 +0100

    Update experiment2.py

--- experiment2.py
+++ experiment2.py
@@ -12,23 +12,23 @@ threshold_op = cytoflow.ThresholdOp(name = "Threshold", channel = "R1-A", thresh
 
 threshold_op.default_view(scale = "logicle", interactive = True).plot(experiment)
 
-experiment2 = threshold_op.apply(experiment)
-experiment2.data.groupby('Threshold').size()
+experiment_threshold = threshold_op.apply(experiment)
+experiment_threshold.data.groupby('Threshold').size()
 
 density_gate_op = cytoflow.DensityGateOp(name = "DensityGate", xchannel = "R1-A", xscale = "logicle", ychannel = "B8-A", yscale = "logicle", keep = 0.5)
 
-density_gate_op.estimate(experiment2)
+density_gate_op.estimate(experiment_threshold)
 
-density_gate_op.default_view().plot(experiment2)
+density_gate_op.default_view().plot(experiment_threshold)
 
-experiment3 = density_gate_op.apply(experiment2)
+experiment_density_gate = density_gate_op.apply(experiment_threshold)
 
 flow_peaks_op = cytoflow.FlowPeaksOp(name = "FlowPeaks", channels = ["R1-A", "B8-A"], scale = {"R1-A" : "logicle", "B8-A" : "logicle"}, h0 = 3)
 
-flow_peaks_op.estimate(experiment3)
+flow_peaks_op.estimate(experiment_density_gate)
 
-flow_peaks_op.default_view(density = True).plot(experiment3)
+flow_peaks_op.default_view(density = True).plot(experiment_density_gate)
 
-experiment4 = flow_peaks_op.apply(experiment3)
+experiment_flow_peaks = flow_peaks_op.apply(experiment_density_gate)
 
-flow_peaks_op.default_view().plot(experiment4)
+flow_peaks_op.default_view().plot(experiment_flow_peaks)
