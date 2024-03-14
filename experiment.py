6fa1176fb06e40f17194db1bf0c556f06c07ed89
immunostep-dev-p <practicas_s@immunostep.com>
2024-03-14 13:56:39 +0100

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -26,33 +26,34 @@ experiment_gaussian_mixture = gaussian_mixture_op.apply(experiment)
 
 gaussian_mixture_op.default_view().plot(experiment_gaussian_mixture, alpha = 0.1)
 
-new_experiment = cytoflow.ImportOp(tubes = [tube]).apply()
+import_op = cytoflow.ImportOp(tubes = [tube], channels = {"R1-A" : "R1-A", "B8-A" : "B8-A"})
+new_experiment = import_op.apply()
 new_experiment
 # print(new_experiment)
 
 flow_peaks_op = cytoflow.FlowPeaksOp(name = "FlowPeaks", channels = ["R1-A", "B8-A"], scale = {"R1-A" : "logicle", "B8-A" : "logicle"}, h0 = 0.8)
 
 flow_peaks_op.estimate(new_experiment)
-new_experiment_flow_peaks_op = flow_peaks_op.apply(new_experiment)
-flow_peaks_op.default_view().plot(new_experiment_flow_peaks_op)
-cytoflow.ScatterplotView(xchannel = "R1-A", xscale = "logicle", ychannel = "B8-A", yscale = "logicle", huefacet = "FlowPeaks").plot(new_experiment_flow_peaks_op)
+new_experiment_flow_peaks = flow_peaks_op.apply(new_experiment)
+flow_peaks_op.default_view().plot(new_experiment_flow_peaks)
+cytoflow.ScatterplotView(xchannel = "R1-A", xscale = "logicle", ychannel = "B8-A", yscale = "logicle", huefacet = "FlowPeaks").plot(new_experiment_flow_peaks)
 
-new_experiment_flow_peaks_op.data
+new_experiment_flow_peaks.data
 # print(new_experiment_flow_peaks.data)
 
-count = new_experiment_flow_peaks_op[["FlowPeaks"]].groupby(by = new_experiment_flow_peaks_op["FlowPeaks"]).count()
+count = new_experiment_flow_peaks[["FlowPeaks"]].groupby(by = new_experiment_flow_peaks["FlowPeaks"]).count()
 count
 # print(count)
 
-umbral = abs(new_experiment_flow_peaks_op["R1-A"].max() / new_experiment_flow_peaks_op["R1-A"].min())
+umbral = abs(new_experiment_flow_peaks["R1-A"].max() / new_experiment_flow_peaks["R1-A"].min())
 umbral
 # print(umbral)
 
-indice = new_experiment_flow_peaks_op["R1-A"].loc[new_experiment_flow_peaks_op["R1-A"] < umbral].index
+indice = new_experiment_flow_peaks["R1-A"].loc[new_experiment_flow_peaks["R1-A"] < umbral].index
 indice
 # print(indice)
 
-cluster = new_experiment_flow_peaks_op[["FlowPeaks"]].drop(indice).groupby(by = new_experiment_flow_peaks_op["FlowPeaks"].drop(indice)).count()
+cluster = new_experiment_flow_peaks[["FlowPeaks"]].drop(indice).groupby(by = new_experiment_flow_peaks["FlowPeaks"].drop(indice)).count()
 cluster
 # print(cluster)
 
@@ -60,7 +61,7 @@ p_cluster = cluster.index[numpy.argmax(cluster.to_numpy())]
 p_cluster
 # print(p_cluster)
 
-bead_cluster = new_experiment_flow_peaks_op[new_experiment_flow_peaks_op["FlowPeaks"] == p_cluster]
+bead_cluster = new_experiment_flow_peaks[new_experiment_flow_peaks["FlowPeaks"] == p_cluster]
 bead_cluster
 # print(bead_cluster)
 
