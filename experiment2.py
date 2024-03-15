62d3f744c21c60418c0437c0fb893cdccc4822a8
immunostep-dev-p <practicas_s@immunostep.com>
2024-03-15 10:42:16 +0100

    Update experiment2.py

--- experiment2.py
+++ experiment2.py
@@ -1,9 +1,10 @@
 import cytoflow
+import numpy
 
 f = r"1.fcs"
 tube = cytoflow.Tube(file = f)
 
-import_op = cytoflow.ImportOp(tubes = [tube], channels = {"R1-A" : "R1-A", "B8-A" : "B8-A"})
+import_op = cytoflow.ImportOp(tubes = [tube], channels = {"R1-A" : "R1-A", "B8-A" : "B8-A", "B4-A" : "B4-A"})
 experiment = import_op.apply()
 experiment
 # print(experiment)
@@ -13,7 +14,7 @@ threshold_op = cytoflow.ThresholdOp(name = "Threshold", channel = "R1-A", thresh
 threshold_op.default_view(scale = "logicle", interactive = True).plot(experiment)
 
 experiment_threshold = threshold_op.apply(experiment)
-# experiment_threshold.data.groupby('Threshold').size()
+# print(experiment_threshold.data.groupby('Threshold').size())
 experiment_threshold = experiment_threshold.query("Threshold")
 
 density_gate_op = cytoflow.DensityGateOp(name = "DensityGate", xchannel = "R1-A", xscale = "logicle", ychannel = "B8-A", yscale = "logicle", keep = 0.5)
@@ -37,3 +38,16 @@ count
 # print(count)
 
 flow_peaks_op.default_view().plot(experiment_flow_peaks)
+
+# Ordenamos los datos
+sorted_data = numpy.sort(experiment_flow_peaks["B4-A"])
+# Obtenemos el número total de datos
+total_number_data = len(sorted_data)
+# Si el número de datos es par
+if total_number_data % 2 == 0:
+    median_fluorescence_intensity = (sorted_data[total_number_data // 2 - 1] + sorted_data[total_number_data // 2]) / 2
+# Si el número de datos es impar
+else:
+    median_fluorescence_intensity = sorted_data[total_number_data // 2]
+print(f"                          Number of events: {experiment_flow_peaks.data.shape[0]}")
+print(f"The Median Fluorescence Intensity (MFI) is: {median_fluorescence_intensity}")
